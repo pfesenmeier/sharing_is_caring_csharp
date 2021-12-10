669f1397123764cea599b9f7e3e5533108fbc938
Paul Fesenmeier <pfesenmeier@gmail.com>
2021-12-10 16:02:39 -0500

    Show try / catch example

--- Program.cs
+++ Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/file-system/how-to-read-from-a-text-file
+class ReadFromFile
+{
+    static void Main()
+    {
+        string text = "";
+        try
+        {
+            // https://docs.microsoft.com/en-us/dotnet/api/system.io.file.readalltext?view=net-6.0
+            text = System.IO.File.ReadAllText(@"./file");
+        }
+        catch
+        {
+            System.Console.WriteLine("Something went wrong reading the file");
+        }
+
+        System.Console.WriteLine("Contents of files.txt = {0}", text);
+    }
+}
